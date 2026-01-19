afa39bbedf80690dcc2a71bddc3a1e666f9b99e7
liuwenbin <775405088@qq.com>
2026-01-19 14:25:02 +0800

    fix: test

--- README.md
+++ README.md
@@ -73,3 +73,10 @@ export default defineConfig([
   },
 ])
 ```
+
+```mermaid
+sequenceDiagram
+Alice->>John: Hello John, how are you?
+John-->>Alice: Great!
+Alice-)John: See you later!
+```
--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig, loadEnv } from "vite";
 import path from "path";
+import viteCompression from "vite-plugin-compression";
 
 // https://vite.dev/config/
 export default defineConfig(({ mode }) => {
@@ -14,6 +15,13 @@ export default defineConfig(({ mode }) => {
       host: true,
     },
     plugins: [
+      viteCompression({
+        algorithm: "gzip", // 使用 gzip 压缩
+        ext: ".gz", // 生成的文件扩展名
+        threshold: 10240, // 仅压缩大于 10KB 的文件
+        deleteOriginFile: false, // 是否删除原始文件
+        compressionOptions: { level: 9 }, // 压缩级别，1-9，越高压缩率越大
+      }),
       react({
         babel: {
           plugins: [["babel-plugin-react-compiler"]],
